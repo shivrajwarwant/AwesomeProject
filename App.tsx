0196e9b4113a6fc0b895d4e61ebf4f8a63650139
shivrajwarwant <shivraj.warwante@proserindustries.com>
2023-08-07 17:40:07 +0530

    hello shivraj warwante

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// eslint-disable-next-line prettier/prettier
+import { Text, View } from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hello, React Native!</Text>
+    </View>
+  );
+};
+
+export default App;
---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+
+import { AppRegistry } from 'react-native';
+import App from './App';
+// eslint-disable-next-line prettier/prettier
+import { name as appName } from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
